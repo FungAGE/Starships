f6cbc5dd15e4800c6457d0a020485c2b009bc881
Adrian Forsythe <adrian.forsythe@ebc.uu.se>
2023-12-21 15:26:05 +0100

    update taxonomy script

--- ships/manual-annotations/format_taxonomy.R
+++ ships/manual-annotations/format_taxonomy.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-starships<-read_tsv("Starships.withtaxa.tsv",col_names=c("Family","Species","Strain","Code","Navis","Size","DR","aTIR","Target","Spok","ARS","Other","HGT","taxID","taxRank","names","ranks")) %>% 
+starships<-read_tsv("Starships.withtaxa.tsv",col_names=c("Family","Species","Strain","Code","Navis","Size","DR","aTIR","Target","Spok","ARS","Other","HGT","taxID","taxRank","names","ranks"),show_col_types = FALSE) %>% 
   separate_rows(ranks,names,sep=";") %>%
   pivot_wider(id_cols=everything(),names_from=ranks,values_from=names,values_fn=function(x) x[1])
   # values_fn=function(x) paste0(unique(x),collapse="; "))
