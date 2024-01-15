35806016b63ff8c7008cf46ecab9cb157bf157bb
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-01-15 12:14:54 +0100

    update paths

--- ships/manual-annotations/set-up.sh
+++ ships/manual-annotations/set-up.sh
@@ -2,23 +2,22 @@
 
 # TODO: store blastdb in SQL
 
-BASEDIR=/home/adrian/Systematics/Starship_Database/starbase/Starships/ships/manual-annotations
-cd $BASEDIR || exit
+BASEDIR=Starships/ships/manual-annotations
 
 # TODO: once tax info is available for mycodb set...
-STARSHIPS=/home/adrian/Systematics/Starship_Database/starbase/SQL/data/fna/ships/manual-annotations
+STARSHIPS=SQL/data/fna/ships/manual-annotations
 # split fasta up
 rm -f $STARSHIPS/Starships.part*
 
-seqkit split -f -i Starships.fa -O $STARSHIPS
+seqkit split -f -i $BASEDIR/Starships.fa -O $STARSHIPS
 
 # fill in taxonomic information
-tail -n +2 Starships.csv | tr "," "\t" | /usr/local/bin/taxonkit name2taxid -i 2 -r > Starships.withtaxaids.tsv
+tail -n +2 $BASEDIR/Starships.csv | tr "," "\t" | /usr/local/bin/taxonkit name2taxid -i 2 -r > $BASEDIR/Starships.withtaxaids.tsv
 
 # FIXME: have to manually add missing taxids here
 
-cat Starships.withtaxaids.tsv | /usr/local/bin/taxonkit lineage -i 14 -R > Starships.withtaxa.tsv
+cat $BASEDIR/Starships.withtaxaids.tsv | /usr/local/bin/taxonkit lineage -i 14 -R > $BASEDIR/Starships.withtaxa.tsv
 
 # creates Starships.fulltaxa.csv
 #TODO: replace spaces and other characters (i.e. parentheses) in species names
-Rscript format_taxonomy.R
+Rscript Starships/scripts/format_taxonomy.R
